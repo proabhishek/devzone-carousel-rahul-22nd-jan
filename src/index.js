a30bba0611285a55f15d0d2e01b10b444f52484c
Abhishek Shankar Choudhary <theabhishek.srm@gmail.com>
2024-01-24 15:26:45 +0530

    index updated

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './views/Home/Home';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <Home/>
 );
+
+window.renderHomeComponent = function(containerId) {
+    const container = document.getElementById(containerId);
+    const root = ReactDOM.createRoot(container);
+    root.render(<Home />);
+};
 
